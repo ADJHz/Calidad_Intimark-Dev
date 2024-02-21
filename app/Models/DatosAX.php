676d9af4c4842d784f952d3d9e39898e47bfa00e
Adolfo U$3R C0D3 <adolfodjhz@gmail.com>
2024-02-21 11:34:15 -0600

    update

---
+++ app/Models/DatosAX.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DatoAX extends Model
+{
+    use HasFactory;
+    protected $table = 'datos_ax';
+
+
+
+}
