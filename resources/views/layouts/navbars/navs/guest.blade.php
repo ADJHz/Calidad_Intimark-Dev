6b43642452a1521b0af647c38356de5f35bc8b7f
Adolfo U$3R C0D3 <adolfodjhz@gmail.com>
2024-02-27 09:19:12 -0600

    update 09:19

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -12,11 +12,6 @@
       </button>
       <div class="collapse navbar-collapse justify-content-end">
         <ul class="navbar-nav">
-          <li class="nav-item{{ $activePage == 'register' ? ' active' : '' }}">
-            <a href="{{ route('register') }}" class="nav-link">
-              <i class="material-icons">person_add</i> {{ __('Registrar') }}
-            </a>
-          </li>
           <li class="nav-item{{ $activePage == 'login' ? ' active' : '' }}">
             <a href="{{ route('login') }}" class="nav-link">
               <i class="material-icons">fingerprint</i> {{ __('Iniciar Sesion') }}
